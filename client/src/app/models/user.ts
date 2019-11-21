fa36a923a3264f9e2db2bb28f4d7c71352e94080
sirpask <el_pask@hotmail.com>
2019-11-21 23:09:26 +0100

    otra

--- client/src/app/models/user.ts
+++ client/src/app/models/user.ts
@@ -1,4 +1,11 @@
 export class User{
+    //es lo mismo que: public _id: string;
+        //ahora esto se puede simplificar
+        //constructor(
+        //  _id: string
+        //) {
+        //  this._id = _id;
+        //  }
 
-    
+
 }
