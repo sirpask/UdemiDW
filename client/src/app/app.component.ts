f7c5086611e6433925f05e08d62ba47894039c0f
sirpask <el_pask@hotmail.com>
2019-11-28 00:07:18 +0100

    otra subida

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css'] NO SE USA
 })
 export class AppComponent {
-  title = 'MUSIFY';
+  public title = 'MUSIFY';
+  public user: User;
+  //local storage
+  public identity //= true;
+  public token;
+
+  constructor(){
+      this.user = new User('','','','','','ROLE_USER','');  // inicializamos el Usuario vacio
+
+
+  }
 }
--- client/src/app/models/user.ts
+++ client/src/app/models/user.ts
@@ -7,5 +7,15 @@ export class User{
         //  this._id = _id;
         //  }
 
+        constructor(
+                public _id: string,
+                public name: string,
+                public surname: string,
+                public email: string,
+                public password: string,
+                public role: string,
+                public image: string
+        ){}
+
 
 }
