c865475bdf10347ae0cb5de5e9f86cb428433c37
sirpask <el_pask@hotmail.com>
2020-01-19 00:34:57 +0100

    otra subida

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -3,4 +3,15 @@ import { Router, RouterModule } from '@angular/router';
 
 
 //import user
-import { UserEditComponent } from 
+import { UserEditComponent } from './components/user-edit.component';
+
+//crear array app Router
+const appRoutes: Routes = [
+  {path: '', component: UserEditComponent},
+  {path: 'mis-datos', component: UserEditComponent},
+  {path: '**', component: UserEditComponent}
+  
+];
+
+export const  appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
